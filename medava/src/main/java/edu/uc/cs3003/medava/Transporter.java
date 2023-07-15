1d4ad360d25bacccb861cf1e6635843e46332146
Andrew Pipo <atpipo2026@gmail.com>
2023-07-14 22:02:02 -0400

    Completed and tested code through Checkpoint 2

--- medava/src/main/java/edu/uc/cs3003/medava/Transporter.java
+++ medava/src/main/java/edu/uc/cs3003/medava/Transporter.java
@@ -6,8 +6,9 @@ import java.util.List;
 public class Transporter {
 
     private String mTransporterName;
+    private double mLowTemperature, mHighTemperature;
 
-    public List<Medicine> goods;
+    private List<Medicine> goods;
     {
         goods = new ArrayList<Medicine>();
     }
@@ -19,5 +20,29 @@ public class Transporter {
     public void ship() {
         // Do some shipping!
     }
+
+    public boolean load(Medicine itemToLoad) {
+        if (itemToLoad.isTemperatureRangeAcceptable(mLowTemperature, mHighTemperature)) {
+            System.out.println(String.format("Adding a %s to the transporter.", itemToLoad.getMedicineName()));
+            goods.add(itemToLoad);
+            return true;
+        }
+        return false;
+    }
+
+    public Transporter(String transporterName, double lowTemp, double highTemp) {
+        mTransporterName = transporterName;
+        mLowTemperature = lowTemp;
+        mHighTemperature = highTemp;
+    }
+
+    public Medicine unload() {
+        return goods.remove(0);
+    }
+    public boolean isEmpty() {
+        return goods.isEmpty();
+    }
+
+
 
 }
