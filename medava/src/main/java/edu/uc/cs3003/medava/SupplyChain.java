55169b0ea17046f7921ac5e6ecd51a829b95c55e
Andrew Pipo <atpipo2026@gmail.com>
2023-07-14 21:54:18 -0400

    Completed and tested code through Checkpoint 1

--- medava/src/main/java/edu/uc/cs3003/medava/SupplyChain.java
+++ medava/src/main/java/edu/uc/cs3003/medava/SupplyChain.java
@@ -2,5 +2,9 @@ package edu.uc.cs3003.medava;
 
 public class SupplyChain {
     public static void main(String args[]) {
+
+        System.out.println("Hello, Andrew Pipo! This is the WOPR speaking.");
+        HospitalRunner.run();
+
     }
 }
