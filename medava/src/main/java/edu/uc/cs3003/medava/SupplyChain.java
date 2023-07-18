338e412c73768c5add0d1290e1b8bf29ac541b28
Andrew Pipo <atpipo2026@gmail.com>
2023-07-18 16:42:06 -0400

    Added questions.txt and zip submission file

--- medava/src/main/java/edu/uc/cs3003/medava/SupplyChain.java
+++ medava/src/main/java/edu/uc/cs3003/medava/SupplyChain.java
@@ -3,7 +3,7 @@ package edu.uc.cs3003.medava;
 public class SupplyChain {
     public static void main(String args[]) {
 
-        System.out.println("Hello, Andrew Pipo! This is the WOPR speaking.");
+        // System.out.println("Hello, Andrew Pipo! This is the WOPR speaking.");
         HospitalRunner.run();
 
     }
